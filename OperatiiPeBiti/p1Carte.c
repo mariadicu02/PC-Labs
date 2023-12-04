bea2f689e475fc86e5d17fe81d3f345e0be1ebf6
Maria <mariadicu02@gmail.com>
2023-12-04 21:40:37 +0200

    setare n biti ai lui x incepand cu valoarea p la cei ultimii n biti din y

--- OperatiiPeBiti/p1Carte.c
+++ OperatiiPeBiti/p1Carte.c
@@ -2,24 +2,11 @@
 #include<stdint.h>
 
 /*Write a function setbits(x,p,n,y) that returns x with the n bits that begin at
-position p set to the rightmost n bits of y, leaving the other bits unchanged. 
+position p set to the rightmost n bits of y, leaving the other bits unchanged.
 Deci am de facut o functie care returneaza x cu cu cei n biti care incep la pozitia p setati la valoarea
 la valoarea ultimilor n biti din dreapta de tot ai lui y, lasand restul bititlor neschimbati.
 */
 
-uint32_t setbits(uint32_t x, int p, int n, uint32_t y) {
-    // Create a mask to clear the bits in the specified range
-    uint32_t mask = ~(~0u << n) << (p - n + 1);
-
-    // Clear the bits in the specified range in x
-    x = x & ~mask;
-
-    // Extract the rightmost n bits from y and position them correctly
-    y = (y & ~(~0u << n)) << (p - n + 1);
-
-    // Combine the modified x and y
-    return x | y;
-}
 
 void print_bit_32(uint32_t nr) {
     uint32_t mask = 0x80000000;
@@ -38,27 +25,35 @@ void print_bit_32(uint32_t nr) {
     printf("\n");
 }
 
+
+uint32_t setbits(uint32_t x, int p, int n, uint32_t y) {
+    // Construiește o mască pentru extragerea celor n biți din y
+    uint32_t mask_y = (1u << n) - 1;
+
+    // Extrage cei n biți din y care trebuie setați în x
+    uint32_t bits_from_y = y & mask_y;
+
+    // Construiește o mască pentru setarea celor n biți în x la poziția p
+    uint32_t mask_set = mask_y << p;
+
+    // Șterge cei n biți din x la poziția p
+    x &= ~mask_set;
+
+    // Setează cei n biți în x la poziția p cu valorile corespunzătoare din y
+    x |= bits_from_y << p;
+
+    return x;
+}
+
 int main() {
-    uint32_t x, y;
-    int p, n;
+    uint32_t x = 0b110110110110;  // Exemplu: 6 biți, 3 setați
+    uint32_t y = 0b101;           // Exemplu: 3 biți, 2 setați
+    int p = 5;                     // Poziția de start (0-indexed)
+    int n = 3;                     // Numărul de biți de modificat
 
-    printf("Enter x (32 bits): ");
-    scanf("%u", &x);
+    printf("x: 0x%x\n", x);
+    printf("y: 0x%x\n", y);
+    printf("setbits result: 0x%x\n", setbits(x, p, n, y));
 
-    printf("Enter y (32 bits): ");
-    scanf("%u", &y);
-
-    printf("Enter p: ");
-    scanf("%d", &p);
-
-    printf("Enter n: ");
-    scanf("%d", &n);
-
-    uint32_t result = setbits(x, p, n, y);
-
-    printf("Result (binary): ");
-    print_bit_32(result);
-
-    printf("Result (decimal): %u\n", result);
     return 0;
 }
